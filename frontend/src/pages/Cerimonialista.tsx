482c30f6e0a3aa4fd722c4357fb0820ea1f61dda
Álvaro Cantos Garcia <alvarocgarcia401@gmail.com>
2026-08-07 16:24:18 -0300

    telas

--- frontend/src/pages/Cerimonialista.tsx
+++ frontend/src/pages/Cerimonialista.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react"
-import { useForm } from "react-hook-form"
 import api from "../service/api"
 
 interface DadosConvidado {
@@ -15,8 +14,6 @@ interface DadosConvidado {
 const Cerimonialista = () => {
   
     const [convidado, setConvidado] = useState<DadosConvidado[]>([])
-
-    const { register, handleSubmit, formState: { errors }, reset, setValue } = useForm<DadosConvidado>()
 
     async function PDF() {
         window.print()
@@ -60,7 +57,18 @@ const Cerimonialista = () => {
 
     async function HandleConvidado(dados: DadosConvidado) {
         try {
-            
+            const token = localStorage.getItem("@Wedding: token")
+            console.log(token)
+
+            await api.patch(`/convidado/pesquisa?nome_completo${dados.nome_completo}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+
+            DadosConvidado()
+
+            alert("Checkin Feito Com Sucesso")
         } catch (error) {
             console.error("Falha ao Criar convidado.", error)
         }
@@ -83,17 +91,17 @@ const Cerimonialista = () => {
     }, [])
 
     return (
-        <main className="bg-amber-100 min-h-screen min-w-screen">
-            <nav className="flex justify-between items-center border-b-amber-400 border-2 h-20 p-2">
+        <main className="bg-amber-100 min-h-screen flex flex-col">
+            <nav className="flex justify-between items-center border-b-3 border-amber-400 h-20 p-2">
                 <h1 className="text-5xl text-green-800">DASHBOARD: CONVIDADO</h1>
                 <div className="flex gap-3">
                     <button onClick={()=>PDF()} className="bg-orange-700 text-white py-2 px-3 rounded-2xl text-2xl hover:bg-orange-600 cursor-pointer">EXPORTAR</button>
                     <button onClick={()=>Sair()} className="bg-orange-700 text-white py-2 px-12 rounded-2xl text-2xl hover:bg-orange-600 cursor-pointer">SAIR</button>
                 </div>
             </nav>
-            <section className=" px-50 flex flex-col items-center gap-5 p-5">
+            <section className="flex flex-col items-center gap-5 py-5">
                 <div className="">
-                    <input className="bg-amber-50 text-[20px] p-2 border-2 rounded-2xl border-amber-400" type="password"/>
+                    <input  className="bg-amber-50 text-[20px] p-2 border-2 rounded-2xl border-amber-400" type="password"/>
                 </div>
                 
                 {convidado.map((c)=>(
